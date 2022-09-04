7f5ab5a5dec54944c558282eecf6c21e10939a74
Gerald Then <gthen@mdsoft.com.do>
2022-09-04 10:51:53 -0400

    Begin With states CRUD

--- GlobalShopping/GlobalShopping/Controllers/CountriesController.cs
+++ GlobalShopping/GlobalShopping/Controllers/CountriesController.cs
@@ -1,5 +1,6 @@
 ﻿using GlobalShopping.Data;
 using GlobalShopping.Data.Entities;
+using GlobalShopping.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -18,7 +19,7 @@ namespace GlobalShopping.Controllers
         public async Task<IActionResult> Index()
         {
             return _context.Countries != null ?
-                        View(await _context.Countries.ToListAsync()) :
+                        View(await _context.Countries.Include(c => c.States).ToListAsync()) :
                         Problem("Entity set 'DataContext.Countries'  is null.");
         }
 
@@ -31,6 +32,7 @@ namespace GlobalShopping.Controllers
             }
 
             var country = await _context.Countries
+                .Include(c => c.States)
                 .FirstOrDefaultAsync(m => m.Id == id);
 
             if (country == null)
@@ -44,7 +46,8 @@ namespace GlobalShopping.Controllers
         // GET: Countries/Create
         public IActionResult Create()
         {
-            return View();
+            Country country = new() { States = new List<State>() };
+            return View(country);
         }
 
         // POST: Countries/Create
@@ -87,11 +90,13 @@ namespace GlobalShopping.Controllers
                 return NotFound();
             }
 
-            var country = await _context.Countries.FindAsync(id);
+            var country = await _context.Countries.Include(c =>c.States).FirstOrDefaultAsync(c => c.Id == id);
+            
             if (country == null)
             {
                 return NotFound();
             }
+
             return View(country);
         }
 
@@ -140,7 +145,10 @@ namespace GlobalShopping.Controllers
                 return NotFound();
             }
 
-            var country = await _context.Countries.FindAsync(id);
+            var country = await _context.Countries
+                .Include(c =>c.States)
+                .FirstOrDefaultAsync(c => c.Id==id);
+
             if (country == null)
             {
                 return NotFound();
@@ -166,6 +174,134 @@ namespace GlobalShopping.Controllers
 
             await _context.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
+        }
+
+        public async Task<IActionResult> AddState(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var country = await _context.Countries.FindAsync(id);
+            if (country == null)
+            {
+                return NotFound();
+            }
+
+            StateViewModel model = new()
+            {
+                CountryId = country.Id,
+            };
+
+            return View(model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> AddState(StateViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+
+                try
+                {
+                    State state = new()
+                    {
+                        Cities = new List<City>(),
+                        Country = await _context.Countries.FindAsync(model.CountryId),
+                        Name = model.Name
+                    };
+                    _context.Add(state);
+                    await _context.SaveChangesAsync();
+                    return RedirectToAction(nameof(Details), new { Id = model.CountryId });
+                }
+                catch (DbUpdateException dbUpdateException)
+                {
+                    if (dbUpdateException.InnerException.Message.Contains("duplicate"))
+                    {
+                        ModelState.AddModelError(string.Empty, "Ya existe una provincia con el mismo nombre en este país.");
+                    }
+                    else
+                    {
+                        ModelState.AddModelError(string.Empty, dbUpdateException.InnerException.Message);
+                    }
+                }
+                catch (Exception exception)
+                {
+                    ModelState.AddModelError(string.Empty, exception.Message);
+                }
+            }
+            return View(model);
+        }
+
+
+        public async Task<IActionResult> EditState(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            State state = await _context.States
+                .Include(s => s.Country)
+                .FirstOrDefaultAsync(s => s.Id == id);
+
+            if (state == null)
+            {
+                return NotFound();
+            }
+
+            StateViewModel model = new()
+            {
+                CountryId = state.Country.Id,
+                Id = state.Id,
+                Name = state.Name,
+            };
+
+            return View(model);
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> EditState(int id, StateViewModel model)
+        {
+            if (id != model.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    State state = new()
+                    {
+                        Id=model.Id,
+                        Name=model.Name,
+                    };
+                    _context.Update(state);
+                    await _context.SaveChangesAsync();
+                    return RedirectToAction(nameof(Details), new {Id =model.CountryId});
+                }
+                catch (DbUpdateException dbUpdateException)
+                {
+                    if (dbUpdateException.InnerException.Message.Contains("duplicate"))
+                    {
+                        ModelState.AddModelError(string.Empty, "Ya existe una provincia con el mismo nombre en este país.");
+                    }
+                    else
+                    {
+                        ModelState.AddModelError(string.Empty, dbUpdateException.InnerException.Message);
+                    }
+                }
+                catch (Exception exception)
+                {
+                    ModelState.AddModelError(string.Empty, exception.Message);
+                }
+            }
+            return View(model);
         }
 
     }
--- GlobalShopping/GlobalShopping/Data/Entities/State.cs
+++ GlobalShopping/GlobalShopping/Data/Entities/State.cs
@@ -2,17 +2,20 @@
 
 namespace GlobalShopping.Data.Entities
 {
-    public class Country
+    public class State
     {
         public int Id { get; set; }
 
-        [Display(Name = "País")]
+        [Display(Name = "Provincia")]
         [MaxLength(50, ErrorMessage = "El campo {0} debe tener máximo {1} caractéres.")]
         [Required(ErrorMessage = "El campo {0} es obligatorio.")]
         public string Name { get; set; }
-        public ICollection<State> States { get; set; }
 
-        [Display(Name = "Provincias")]
-        public int StatesNumber => States == null ? 0 : States.Count;
+        public Country Country { get; set; }
+
+        public ICollection<City> Cities { get; set; }
+
+        [Display(Name = "Ciudades")]
+        public int CitiesNumber => Cities == null ? 0 : Cities.Count;
     }
 }
