3ad6effa1b46c2ae5a4187b52c09693d7384077e
Gerald Then <gthen@mdsoft.com.do>
2022-09-11 16:35:59 -0400

    Add Products

--- GlobalShopping/GlobalShopping/Data/DataContext.cs
+++ GlobalShopping/GlobalShopping/Data/DataContext.cs
@@ -13,6 +13,9 @@ namespace GlobalShopping.Data
         public DbSet<Category> Categories { get; set; }
         public DbSet<City> Cities { get; set; }
         public DbSet<Country> Countries { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductCategory> ProductCategories { get; set; }
+        public DbSet<ProductImage> ProductImages { get; set; }
         public DbSet<State> States { get; set; }
         
 
@@ -23,6 +26,8 @@ namespace GlobalShopping.Data
             modelBuilder.Entity<Category>().HasIndex(c => c.Name).IsUnique();
             modelBuilder.Entity<State>().HasIndex("Name", "CountryId").IsUnique();
             modelBuilder.Entity<City>().HasIndex("Name", "StateId").IsUnique();
+            modelBuilder.Entity<Product>().HasIndex(p => p.Name).IsUnique();
+            modelBuilder.Entity<ProductCategory>().HasIndex("ProductId", "CategoryId").IsUnique();
         }
     }
 }
---
+++ GlobalShopping/GlobalShopping/Data/Entities/ProductImage.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GlobalShopping.Data.Entities
+{
+	public class ProductImage
+	{
+        public int Id { get; set; }
+
+        public Product Product { get; set; }
+
+        [Display(Name = "Foto")]
+        public Guid ImageId { get; set; }
+
+        //TODO: Pending to change to the correct path
+        [Display(Name = "Foto")]
+        public string ImageFullPath => ImageId == Guid.Empty
+            ? $"https://localhost:7272/images/noimg.png"
+            : $"https://localhost:7272/images/products/{ImageId}.png";
+
+    }
+}
